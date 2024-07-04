b66af46cde8d5eca9cf3980bafbaa5aa7b1fff2a
HACINE11 <132098004+HACINE11@users.noreply.github.com>
2024-07-04 16:50:41 +0100

    Verification Email : verifyEmail

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { HttpClient } from  '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { User } from '../models/user';
+import { map, catchError } from 'rxjs/operators';
 
 
 
@@ -25,7 +26,27 @@ export class AuthServiceService {
      
   }
   signUp(user: User): Observable<any> {
-    return this.http.post<any>(`http://localhost:9090/user/signup`, user);
+    return this.http.post<any>(`http://localhost:9090/user/signup`, user)
+    .pipe(
+      map(response => {
+        this.router.navigate(['/verify-email']);
+        return { isOk: true };
+      }),
+      catchError(error => {
+        return of({ isOk: false, message: "Failed to create account" });
+      })
+    );
   }
-
+  verifyAccount(email: string, code: string): Observable<any> {
+    return this.http.post<any>('http://localhost:9090/user/verify', { email, code })
+      .pipe(
+        map(response => {
+          this.router.navigate(['/login']);
+          return { isOk: true };
+        }),
+        catchError(error => {
+          return of({ isOk: false, message: "Failed to verify account" });
+        })
+      );
+  }
 }
